d0b48e43ccd0e7eef7c844bc386490eb7a4c0e88
TheFatAndFurious <mrguerrilla@gmail.com>
2025-03-16 23:54:04 +0100

    async connection to database

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,11 +1,26 @@
+from contextlib import asynccontextmanager
+
+import psycopg
 from fastapi import FastAPI
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.db = psycopg.AsyncConnection.connect(
+    host="postgres",
+            database="pgdb",
+            user="pguser",
+            password="pgpassword"
+    )
+    print("Connected to db")
+    yield
+    await app.state.db.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "bruh bruh"}
-
 
 
