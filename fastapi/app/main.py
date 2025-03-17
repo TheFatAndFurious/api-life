7defaf69097598d48b68167e9ac8041ba1982ce1
TheFatAndFurious <mrguerrilla@gmail.com>
2025-03-17 15:35:01 +0100

    pooling

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,34 +1,40 @@
 from contextlib import asynccontextmanager
 
 import psycopg
+import psycopg_pool
 from fastapi import FastAPI
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.db = await psycopg.AsyncConnection.connect(
+    app.state.pool = await psycopg_pool.AsyncConnectionPool.connect(
             host="postgres",
             dbname="pgdb",
             user="pguser",
-            password="pgpassword"
+            password="pgpassword",
+            min_size=5,
+            max_size=20
     )
-    print("Connected to db")
+    print("Pool opened")
     yield
-    await app.state.db.close()
+    await app.state.pool.close()
+    print("Pool closed")
 
 
 app = FastAPI(lifespan=lifespan)
 
+#TODO: add pooling
+#TODO: create connection using ORM
 
 @app.post("/items")
 async def read_root():
-    async with app.state.db.cursor() as cursor:
+    async with app.state.pool.cursor() as cursor:
         await cursor.execute("INSERT INTO items (name, description) VALUES (%s, %s)", ("matt", "larnak"))
         await app.state.db.commit()
         return {"success": True}
 
 @app.get("/items")
 async def get_all_items():
-    async with app.state.db.cursor() as cursor:
+    async with app.state.pool.cursor() as cursor:
         await cursor.execute("SELECT * FROM items")
         items = await cursor.fetchall()
         return {"items": items}
